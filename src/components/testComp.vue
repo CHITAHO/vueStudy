c885733b0cbbc66ccb1d39055c11c4a5db9fb679
thchoi <xogh2120@gmail.com>
2024-01-22 09:50:35 +0900

    청첩장 추가

--- src/components/testComp.vue
+++ src/components/testComp.vue
@@ -1,11 +1,12 @@
 <!-- MenuBar.vue -->
 <template>
     <div class="menu-bar">
+        <input v-model="userInput" placeholder="닉네임을 입력하세요" @keyup.enter="pressEnter">
         <ul>
-            <li><a @click="clickMenu1">메뉴 항목 1</a></li>
-            <li><a @click="clickMenu2">메뉴 항목 2</a></li>
-            <li><a @click="clickMenu3">메뉴 항목 3</a></li>
+            <li><a @click="clickMenu2">본캐 확인</a></li>
         </ul>
+        <h2>{{ this.union?.character_name }}</h2>
+        <img :src="imgSrc">
     </div>
 </template>
   
@@ -14,30 +15,20 @@
 export default {
     data() {
         return {
-            name: "듀블타이호",
             ocid: undefined,
             union: undefined,
+            userInput: '',
+            imgSrc: '',
+            date: '',
         }
     },
+    mounted() {
+        this.getParamDate();
+    },
     methods: {
-        clickMenu1() {
-            var myHeaders = new Headers();
-            myHeaders.append("x-nxopen-api-key", "test_d4d23de7e6fac346bd22bd4ab92546054630f3d320bd9b6296d237328f5e0731aabac9b3aa005976c53593c2554ef6f9");
-
-            var requestOptions = {
-                method: 'GET',
-                headers: myHeaders,
-                redirect: 'follow'
-            };
-
-            fetch(`https://open.api.nexon.com/maplestory/v1/id?character_name=${this.name}`, requestOptions)
-                .then(response => response.text())
-                .then(result => {
-                    this.ocid = JSON.parse(result).ocid;
-                    alert(this.ocid)
-                }
-                )
-                .catch(error => console.log('error', error));
+        async pressEnter() {
+            // 엔터 키 누를 때 실행할 메서드를 호출합니다.
+            await this.clickMenu2();
         },
         async clickMenu2() {
 
@@ -49,19 +40,44 @@ export default {
                 headers: myHeaders,
                 redirect: 'follow'
             };
-            await fetch(`https://open.api.nexon.com/maplestory/v1/ranking/union?&date=2023-12-26&ocid=${this.ocid}`, requestOptions)
+            await fetch(`https://open.api.nexon.com/maplestory/v1/id?character_name=${this.userInput}`, requestOptions)
                 .then(response => response.text())
                 .then(result => {
-                    this.union = JSON.parse(result).ranking[0]
-                    console.log(this.union)
+                    this.ocid = JSON.parse(result).ocid;
                 }
                 )
                 .catch(error => console.log('error', error));
-
-            alert(this.union.character_name)
+            await fetch(`https://open.api.nexon.com/maplestory/v1/ranking/union?&date=${this.date}&ocid=${this.ocid}`, requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    this.union = JSON.parse(result).ranking[0]
+                }
+                )
+                .catch(error => console.log('error', error));
+            await fetch(`https://open.api.nexon.com/maplestory/v1/character/basic?ocid=${this.ocid}&date=${this.date}`, requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    console.log(result)
+                }
+                )
         },
-        clickMenu3() {
-            alert("메뉴항목 3 클릭")
+        getParamDate() {
+            // 현재 날짜를 얻습니다.
+            const currentDate = new Date();
+
+            // 현재 날짜에서 하루를 빼서 어제의 날짜를 설정합니다.
+            const yesterdayDate = new Date();
+            yesterdayDate.setDate(currentDate.getDate() - 1);
+            // 날짜를 원하는 형식으로 변환합니다 (yy-mm-dd 형식).
+            return this.formatDate(yesterdayDate);
+        },
+        formatDate(date) {
+            const year = date.getFullYear().toString().slice(2);
+            const month = ('0' + (date.getMonth() + 1)).slice(-2);
+            const day = ('0' + date.getDate()).slice(-2);
+
+            // this.date = `${year}-${month}-${day}`;
+            this.date = year + "-" + month + "-" + day;
         }
     }
 }
---
+++ src/views/MerryView.vue
@@ -0,0 +1,210 @@
+<template>
+    <div id="wrap">
+        <div class="loader_main_preview">
+            <div class="lds-ellipsis">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        <div class="frame_wrap" style="display: block;">
+            <!-- 표지 디자인 (메인 디자인) -->
+            <div id="area1" idx="1" class="templatearea" style="display: block; height: 1423px;">
+                <div id="item_1" class="item" style="top: 0px; left: 0px;  position:absolute;"><img class="img"
+                        src="https://resources.barunsonmcard.com/upload/template/MC1224/8742e454-2ab1-45df-a3fb-823da18af8a7.png"
+                        width="800px" height="1423px"></div>
+                <div id="item_2" class="item photo-image" style="top: 318px; left: 108px;  position:absolute;"><img
+                        class="img"
+                        src="https://resources.barunsonmcard.com/upload/template/MC1224/39a27e19-9209-40fd-8572-0cdbc859e6ff.png"
+                        width="588.625px" height="545.844px"></div>
+                <div id="item_3" class="item"
+                    style="top: 938px; left: 0.515625px; position: absolute; width: 368.797px; height: 82px; align-items: center;">
+                    <div class="text"
+                        style="font-family: &quot;Nanum Myeongjo&quot;, serif; font-size: 55px; color: rgb(51, 51, 51); text-align: right; letter-spacing: 0em; line-height: 1.4em;">
+                        이준정</div>
+                </div>
+                <div id="item_4" class="item"
+                    style="top: 937px; left: 431.516px; position: absolute; width: 368.797px; height: 82px; align-items: center;">
+                    <div class="text"
+                        style="font-family: &quot;Nanum Myeongjo&quot;, serif; font-size: 55px; color: rgb(51, 51, 51); text-align: left; letter-spacing: 0em; line-height: 1.4em;">
+                        김유은</div>
+                </div>
+                <div id="item_5" class="item"
+                    style="top: 1032px; left: 1.51562px; position: absolute; width: 798.891px; height: 52px; align-items: center;">
+                    <div class="text"
+                        style="font-family: &quot;Nanum Myeongjo&quot;, serif; font-size: 35px; color: rgb(51, 51, 51); text-align: center; letter-spacing: 0em; line-height: 1.4em;">
+                        2024. 02. 03. SAT PM 01:30</div>
+                </div>
+                <div id="item_6" class="item"
+                    style="top: 1080px; left: 0px; position: absolute; width: 800px; height: 52px; align-items: center;">
+                    <div class="text"
+                        style="font-family: &quot;Nanum Myeongjo&quot;, serif; font-size: 35px; color: rgb(51, 51, 51); text-align: center; letter-spacing: 0em; line-height: 1.4em;">
+                        메종 드 프리미어 그랜드홀</div>
+                </div>
+                <div id="item_7" class="item"
+                    style="top: 948px; left: 389.516px; position: absolute; width: 25.6875px; height: 82px; align-items: center;">
+                    <div class="text"
+                        style="font-family: &quot;Noto Sans KR&quot;, sans-serif; font-size: 55px; color: rgb(0, 0, 0); text-align: left; letter-spacing: 0em; line-height: 1.4em;">
+                        *</div>
+                </div>
+            </div>
+            <!-- 초대글 (인사말) -->
+            <div id="area2" idx="2" class="templatearea"
+                style="display: block; height: 889px; background-color: rgb(241, 241, 241);">
+                <div id="item_8" class="item" style="top: 0px; left: 0px;  position:absolute;"><img class="img"
+                        src="https://resources.barunsonmcard.com/upload/template/MC1224/49706675-f065-4971-845f-32b2272ba3b4.png"
+                        width="800px" height="175px"></div>
+                <div id="item_9" class="item"
+                    style="top: 297px; left: 0px; position: absolute; width: 800px; height: 469.125px; align-items: center;">
+                    <div class="text"
+                        style="font-family: &quot;Nanum Myeongjo&quot;, serif; font-size: 34px; color: rgb(51, 51, 51); text-align: center; letter-spacing: 0em; line-height: 2.3em;">
+                        서로가 마주보며 다져온 사랑을<br>이제 함께 한 곳을 바라보며<br>걸어갈 수 있는 큰 사랑으로 키우고자 합니다.<br>저희 두 사람이 사랑의 이름으로<br>지켜나갈 수 있게
+                        앞날을<br>축복해 주시면 감사하겠습니다.</div>
+                </div>
+            </div>
+            <!-- 신랑,신부 연락처 타이틀 이미지 -->
+            <div id="area3" idx="3" class="templatearea" style="display: block;"></div>
+            <!-- 신랑 신부 연락정보 -->
+            <div id="area4" idx="4" class="call templatearea" style="display: block;"><img
+                    src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/img_01.png"
+                    style="width: 800px; display: block;">
+                <div class="list_wrap" style="margin-top: 0px;"><img
+                        src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/img_02.png"
+                        style="width: 800px; display: block;"></div>
+            </div>
+            <!-- 돌잔치 -->
+            <!-- 아빠 엄마 연락처 -->
+            <div id="area21" idx="21" class="templatearea call line" style="display: none;"><img
+                    src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/tell_PCC03.png"
+                    style="width: 800px; display: block;">
+                <div class="list_wrap"></div>
+            </div>
+            <!-- 아기정보 타이틀 이미지 -->
+            <div id="area17" idx="17" class="templatearea" style="display: none;"></div>
+            <!-- 아기 정보 프로필 디자인 -->
+            <div id="area18" idx="18" class="templatearea" style="display: none;"></div>
+            <!-- 아기 정보 본문 디자인 -->
+            <div id="area19" idx="19" class="templatearea" style="display: none;"></div>
+            <!-- 아기 정보 하단 이미지 -->
+            <div id="area20" idx="20" class="templatearea" style="display: none;"></div>
+            <!-- 돌잔치 -->
+            <!-- 갤러리 타이틀 이미지 -->
+            <div id="area5" idx="5" class="templatearea" style="display: block; height: 181px;">
+                <div id="item_10" class="item" style="top: 0px; left: 0px;  position:absolute;"><img class="img"
+                        src="https://resources.barunsonmcard.com/upload/template/MC1224/17a364ba-5163-4576-92f9-ecb1f85bc779.png"
+                        width="800px" height="181px"></div>
+            </div>
+            <!-- 갤러리 -->
+            <div id="area6" idx="6">
+                <img src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/img_03.png"
+                    style="width: 800px; display: block;">
+            </div>
+            <!-- 동영상 타이틀 이미지 -->
+            <div id="area7" idx="7" class="templatearea" style="display: block; height: 181px;">
+                <div id="item_11" class="item" style="top: 0px; left: 0px;  position:absolute;"><img class="img"
+                        src="https://resources.barunsonmcard.com/upload/template/MC1224/8662cd82-fae7-4c93-9a46-131d83448f7f.png"
+                        width="800px" height="181px"></div>
+            </div>
+            <!-- 동영상 -->
+            <div id="area8" idx="8" class="movie line">
+                <img src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/img_04.png"
+                    style="width: 800px; display: block;">
+            </div>
+            <!-- 오시는길 타이틀 이미지 -->
+            <div id="area9" idx="9" class="templatearea" style="display: block; height: 181px;">
+                <div id="item_12" class="item" style="top: 0px; left: 0px;  position:absolute;"><img class="img"
+                        src="https://resources.barunsonmcard.com/upload/template/MC1224/5db8a158-a8e3-47da-91fb-14464a010eed.png"
+                        width="800px" height="181px"></div>
+            </div>
+            <!-- 오시는 길 -->
+            <div id="area10" idx="10" class="location line">
+                <img src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/img_05.png"
+                    style="width: 800px; display: block;">
+            </div>
+            <div class="flowergift" style="display: block;">
+                <img src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/img_flowergift_v2.png"
+                    style="width: 800px;">
+            </div>
+            <div class="remittance onoff_4">
+                <img src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/img_06.png"
+                    style="width: 800px; display: block;">
+            </div>
+
+            <!-- 방명록 타이틀 이미지 -->
+            <div id="area14" idx="14" class="templatearea"
+                style="display: block; height: 227px; background-color: rgb(247, 248, 249);">
+                <div id="item_14" class="item" style="top: 0px; left: 0px;  position:absolute;"><img class="img"
+                        src="https://resources.barunsonmcard.com/upload/template/MC1224/5fc69ac4-93d2-49ef-97c8-b43f67e3cdaf.png"
+                        width="800px" height="227px"></div>
+            </div>
+            <!-- 방명록 -->
+            <div id="area15">
+                <img src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/message.png"
+                    style="width: 800px; display: block;">
+            </div>
+            <!-- //방명록 -->
+            <!-- footer -->
+            <div id="area16">
+                <img src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/share.png"
+                    style="width: 800px; display: block;"><img
+                    src="https://static.barunsoncard.com/barunsonmcard/invitation/preview/barunson.png"
+                    style="width: 800px; display: block;">
+            </div>
+            <!-- //footer -->
+
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            // Add any data properties if needed
+        };
+    },
+    // Add methods or lifecycle hooks if needed
+};
+</script>
+  
+<style>
+/* Add your styles here */
+#wrap {
+    overflow: hidden;
+}
+
+.templatearea {
+    width: 100%;
+    position: relative;
+    letter-spacing: 0px;
+}
+
+.item {
+    display: flex;
+}
+
+.text {
+    width: 100%;
+}
+
+.photo {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.frame_wrap {
+    display: none;
+    background: #fff;
+}
+
+#wrap {
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+/* Add other styles as needed */</style>
+  
